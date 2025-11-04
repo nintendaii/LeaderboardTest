d7e4e531b5a7406ca8b1dbf89c6061b4a5cbd405
Daniel <44233090+nintendaii@users.noreply.github.com>
2025-11-04 14:45:46 +0200

    Added sorting score

--- Assets/Module.App/Scripts/CommandSignal/CommandOpenLeaderboardPopup.cs
+++ Assets/Module.App/Scripts/CommandSignal/CommandOpenLeaderboardPopup.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Module.App.Scripts.Data;
 using Module.App.Scripts.Factory;
 using Module.App.Scripts.Services;
 using Module.Common.Scripts;
@@ -15,7 +17,19 @@ namespace Module.App.Scripts.CommandSignal
         public async void Execute()
         {
             var data = await _leaderboardService.LoadAsync();
+            SortLeaderboard(data);
             _signalBus.Fire(new SignalOpenPopup(GlobalConstants.Addressable.LEADERBOARD_ADDRESSABLE_PATH, data));
+        }
+        
+        /// <summary>
+        /// Sorts the list by score value. The sort algorithm has O(n log n) complexity which is the best possible complexity for sorting  
+        /// </summary>
+        /// <param name="records">List of loaded records from JSON</param>
+        private void SortLeaderboard(List<LeaderboardRecordData> records)
+        {
+            if (records == null || records.Count <= 1) return;
+
+            records.Sort((a, b) => b.score.CompareTo(a.score)); //O(n log n) complexity
         }
     }
 }
